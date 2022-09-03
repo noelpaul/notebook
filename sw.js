de8f7f394f2a0e880cfb66c3ea2b9b85fc500889
noelpaul <rosytomy101@gmail.com>
2022-09-03 15:23:45 +0530

    new notebook

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 1.050;
+const version = 1.051;
 const staticCacheKey = `shell-assets-version-${version}`;
 const dynamicCacheKey = `extra-assets-version-${version}`;
 const dynamicCacheLimit = 15;
